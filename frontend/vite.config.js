3ac10eaa18b4a7264008d29322034eac1ad60375
rk <rk@163.com>
2026-06-07 21:13:06 +0800

    个re

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3085,
+    port: 3083,
     host: '0.0.0.0',
     proxy: {
       '/api': {
